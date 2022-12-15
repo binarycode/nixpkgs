a09e4cd96b219e21885c0877f3b8fd0bce2033ba
Igor Sidorov <igor.sidorov@binarycode.ru>
2022-12-15 21:56:43 +0300

    skip patch 01

--- pkgs/development/interpreters/ruby/patchsets.nix
+++ pkgs/development/interpreters/ruby/patchsets.nix
@@ -9,7 +9,6 @@
     "${patchSet}/patches/ruby/2.7/head/railsexpress/05-malloc-trim.patch"
   ];
   "3.0.5" = ops useRailsExpress [
-    "${patchSet}/patches/ruby/3.0/head/railsexpress/01-fix-with-openssl-dir-option.patch"
     "${patchSet}/patches/ruby/3.0/head/railsexpress/02-improve-gc-stats.patch"
     "${patchSet}/patches/ruby/3.0/head/railsexpress/03-malloc-trim.patch"
   ];
